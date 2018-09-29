471a80545d93641461f43e8b683a1065ebf872b9
zvzuola <15257109820@163.com>
2018-09-29 15:27:34 +0800

    feat: 框架搭建

---
+++ src/api/api.js
@@ -0,0 +1,38 @@
+import wepy from 'wepy'
+import CryptoJS from 'crypto-js'
+
+const UID = "UAA44ECA5D";
+const KEY = "ikllxqdufpsrynqf";
+
+const getWeatherUrl = (location = 'hangzhou', type ='daily') => {
+  const api = `https://api.seniverse.com/v3/weather/${type}.json`;
+  const ts = Math.floor((new Date()).getTime() / 1000);
+  const str = `ts=${ts}&uid=${UID}`;
+  const sig = encodeURIComponent(CryptoJS.HmacSHA1(str, KEY).toString(CryptoJS.enc.Base64));
+  const url = `${api}?location=${location}&${str}&sig=${sig}&start=0&days=3`;
+  return url;
+}
+
+const wxRequest = async (url, params = {}) => {
+  wepy.showToast({
+    title: '加载中',
+    icon: 'loading'
+  })
+  let res = await wepy.request({
+    url: url,
+    method: params.method || 'GET',
+    data: params.data || {},
+    header: { 'Content-Type': 'application/json' }
+  })
+  wepy.hideToast()
+  console.log(url, res)
+  return res
+}
+
+export const getDailyWeather = (location = 'hangzhou') => {
+  return wxRequest(getWeatherUrl(location, 'daily'))
+}
+
+export const getNowWeather = (location = 'hangzhou') => {
+  return wxRequest(getWeatherUrl(location, 'now'))
+}
